4f8641b2b9e8543b439730fd34f1e6175a6e8f09
Arxeno <rmfbkw04@gmail.com>
2024-07-17 20:37:49 +0700

    fix!: change default fonts to poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 import Layout from '@/components/elements/Layout'
 import Navbar from '@/components/elements/Navbar'
 import Footer from '@/components/elements/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Layout>
           <Navbar />
           {children}
